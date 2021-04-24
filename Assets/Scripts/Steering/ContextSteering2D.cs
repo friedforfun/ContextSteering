39a1c6e5fa2872e6af419ba6c955d4087ac565d6
Sam Fay-Hunt <samfayhunt@gmail.com>
2021-04-24 22:30:14 +0100

    Basic steering script

--- Assets/Scripts/Steering/ContextSteering2D.cs
+++ Assets/Scripts/Steering/ContextSteering2D.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using System.Collections.Concurrent;
+using System.Linq;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -15,12 +18,20 @@ public enum BuildContextMapOn
 public class Steering2D : MonoBehaviour
 {
     [Header("Behaviours")]
-    public float Radius;
-    public ISteeringBehaviour[] SteeringBehaviours;
+    public int ContextMapResolution; // Number of directions the context map represents
+    public ISteeringBehaviour[] SteeringBehaviours; // Attractors and Repulsors
     public ISteeringMask[] SteeringMasks;
     public BuildContextMapOn BuildMapOn;
 
-    private float TimeInterval;
+    [Header("Gizmo")]
+    [SerializeField] private float Radius;
+
+    private float resolutionAngle; // Each point is separeted by a some degrees rotation (360/len(chaseMap))
+
+    private void Start()
+    {
+        resolutionAngle = 360 / (float)ContextMapResolution;
+    }
 
     private void Update()
     {
@@ -36,6 +47,56 @@ public class Steering2D : MonoBehaviour
         {
             return;
         }
+    }
+
+    
+    /// <summary>
+    /// Builds a context map of directions towards the target from all steering behaviours
+    /// </summary>
+    /// <returns></returns>
+    private float[] buildSteeringBehaviours()
+    {
+        ConcurrentBag<float[]> contextMaps = new ConcurrentBag<float[]>();
+
+        Parallel.ForEach(SteeringBehaviours, behaviour =>
+        {
+            contextMaps.Add(behaviour.BuildContextMap());
+        });
+        
+        return mergeContextMaps(contextMaps);
+    }
+
+    /// <summary>
+    /// Build a context mask of directions to block movement in this direction
+    /// </summary>
+    /// <returns></returns>
+    private float[] buildSteeringMasks()
+    {
+        ConcurrentBag<float[]> contextMaps = new ConcurrentBag<float[]>();
+
+        Parallel.ForEach(SteeringMasks, behaviour =>
+        {
+            contextMaps.Add(behaviour.BuildContextMask());
+        });
+
+        return mergeContextMaps(contextMaps);
+    }
+
+    /// <summary>
+    /// Merge bag of context maps
+    /// </summary>
+    /// <param name="maps"></param>
+    /// <returns></returns>
+    private float[] mergeContextMaps(ConcurrentBag<float[]> maps)
+    {
+        float[] contextMap = new float[ContextMapResolution];
+
+        foreach (float[] map in maps)
+        {
+            contextMap.Zip(map, (x, y) => x + y);
+        }
+
+        return contextMap;
     }
 
 
