84016a080bd7b8aa52c1b7a8f9ceb3741806d891
Sam Fay-Hunt <samfayhunt@gmail.com>
2021-05-01 12:53:39 +0100

    Target teleporter

---
+++ Assets/Scripts/TestLevel/BoxPorter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxPorter : MonoBehaviour
+{
+    [SerializeField] private Vector3[] jumpPoints;
+    private int jumpIndex = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Agent"))
+        {
+            if (jumpIndex == jumpPoints.Length)
+            {
+                jumpIndex = 0;
+            }
+            transform.position = jumpPoints[jumpIndex];
+            jumpIndex++;
+        }
+    }
+}
