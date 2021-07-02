80e07ca89170d60a94b4ce515c3aa80c87fbf61e
Sam Fay-Hunt <36762577+friedforfun@users.noreply.github.com>
2021-07-02 20:44:51 +0100

    Removed fps stuff

--- Assets/Scripts/Steering/DOTS/BaseSteering/SteeringScheduler.cs
+++ Assets/Scripts/Steering/DOTS/BaseSteering/SteeringScheduler.cs
@@ -122,24 +122,6 @@ namespace Friedforfun.SteeringBehaviours.Core2D.Buffered
             CompleteBehavioursTest();
         }
 
-        const float fpsMeasurePeriod = 0.5f;
-        private int m_FpsAccumulator = 0;
-        private float m_FpsNextPeriod = 0;
-        private int m_CurrentFps = 0;
-
-        public int Measure()
-        {
-            // measure average frames per second
-            m_FpsAccumulator++;
-            if (Time.realtimeSinceStartup > m_FpsNextPeriod)
-            {
-                m_CurrentFps = (int)(m_FpsAccumulator / fpsMeasurePeriod);
-                m_FpsAccumulator = 0;
-                m_FpsNextPeriod += fpsMeasurePeriod;
-            }
-            return m_CurrentFps;
-        }
-
     }
 
 }
