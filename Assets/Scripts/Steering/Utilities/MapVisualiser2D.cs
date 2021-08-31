b58095ccd5a3594da974dcb9627a78d605a82898
Sam Fay-Hunt <36762577+friedforfun@users.noreply.github.com>
2021-08-31 22:16:38 +0100

    Added steering map debugger

---
+++ Assets/Scripts/Steering/Utilities/MapVisualiser2D.cs
@@ -0,0 +1,58 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace Friedforfun.SteeringBehaviours.Utilities
+{
+    [System.Serializable]
+    public class MapVisualiserParameters
+    {
+        [Tooltip("Enable/Disable debug visuals")]
+        [SerializeField] public bool ShowDebug = false;
+        [Tooltip("The Radius of the debug map to be drawn (useful for overlapping maps).")]
+        [SerializeField] public float MapSize = 2f;
+        [Tooltip("The colour of the debug map that will be drawn.")]
+        [SerializeField] public Color DebugColor = Color.red;
+    }
+
+    public class MapVisualiser2D 
+    {
+
+
+    #if UNITY_EDITOR
+        /// <summary>
+        /// Call this inside OnDrawGizmos in the steering behaviour or mask you wish to visualise.
+        /// </summary>
+        public void InDrawGizmos(MapVisualiserParameters MapParams, float[] contextMap, float range, float resolutionAngle, Transform transform)
+        {
+            bool ShowDebug = MapParams.ShowDebug;
+            float MapSize = MapParams.MapSize;
+            Color DebugColor = MapParams.DebugColor;
+
+            if (!ShowDebug || contextMap is null || contextMap.Length == 0)
+            {
+                return;
+            }
+
+            Vector3 position = transform.position;
+            Handles.DrawWireDisc(position, Vector3.up, range);
+
+            position = new Vector3(position.x, position.y + 0.1f, position.z);
+            Vector3 direction = Vector3.forward;
+
+            foreach (float weight in MapOperations.NormaliseMap(contextMap, MapSize))
+            {
+                Gizmos.color = DebugColor;
+                Gizmos.DrawRay(transform.position, direction * weight);
+                direction = Quaternion.Euler(0f, resolutionAngle, 0) * direction;
+            }
+            Handles.color = DebugColor;
+            Handles.DrawWireDisc(position, Vector3.up, MapSize);
+
+        }
+    #endif
+    }
+
+}
+
