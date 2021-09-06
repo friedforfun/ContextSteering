884dcf41dcedc0959bcb2354b5c3ac0402206097
Sam Fay-Hunt <36762577+friedforfun@users.noreply.github.com>
2021-09-06 19:47:25 +0100

    Moved enums into core

---
+++ Assets/Scripts/Refactor_steering/Core/CoreEnums.cs
@@ -0,0 +1,17 @@
+
+
+namespace Friedforfun.SteeringBehaviours.Core
+{
+    public enum SteerDirection
+    {
+        ATTRACT,
+        REPULSE
+    }
+
+    public enum RotationAxis
+    {
+        XAxis,
+        YAxis,
+        ZAxis
+    }
+}
