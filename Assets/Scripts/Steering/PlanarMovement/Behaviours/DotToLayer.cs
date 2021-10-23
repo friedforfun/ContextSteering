1a4157bea6eb2926654ff483dbce30c83bf32e7f
Sam Fay-Hunt <36762577+friedforfun@users.noreply.github.com>
2021-10-23 02:42:12 +0100

    Added DotToLayer behaviour

---
+++ Assets/Scripts/Steering/PlanarMovement/Behaviours/DotToLayer.cs
@@ -0,0 +1,32 @@
+using Unity.Collections;
+using UnityEngine;
+using Friedforfun.SteeringBehaviours.Core;
+using Friedforfun.SteeringBehaviours.Utilities;
+using System.Collections.Generic;
+using System;
+
+namespace Friedforfun.SteeringBehaviours.PlanarMovement
+{
+    public class DotToLayer : PlanarSteeringBehaviour
+    {
+
+        [SerializeField] LayerMask Layers;
+
+        protected override Vector3[] getPositionVectors()
+        {
+            var collisionList = new List<Vector3>();
+            Collider[] checkLayers = Physics.OverlapSphere(transform.position, Range, Layers);
+            if (checkLayers != null)
+            {
+                foreach (Collider collision in checkLayers)
+                {
+                    collisionList.Add(collision.ClosestPoint(transform.position));
+                }
+            }
+
+            return collisionList.ToArray();
+        }
+
+    }
+
+}
