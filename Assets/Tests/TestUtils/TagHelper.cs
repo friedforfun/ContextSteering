39b966076a964b54626ca0c1551f38330faeb03e
Sam Fay-Hunt <36762577+friedforfun@users.noreply.github.com>
2021-10-24 02:42:38 +0100

    Added TagHelper to add/remove tags in tests

---
+++ Assets/Tests/TestUtils/TagHelper.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Friedforfun.SteeringBehaviours.Tests
+{
+    public static class TagHelper
+    {
+        // from here: https://answers.unity.com/questions/33597/is-it-possible-to-create-a-tag-programmatically.html
+        public static void AddTag(string tag)
+        {
+            UnityEngine.Object[] asset = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset");
+            if ((asset != null) && (asset.Length > 0))
+            {
+                SerializedObject so = new SerializedObject(asset[0]);
+                SerializedProperty tags = so.FindProperty("tags");
+
+                for (int i = 0; i < tags.arraySize; ++i)
+                {
+                    if (tags.GetArrayElementAtIndex(i).stringValue == tag)
+                    {
+                        return;     // Tag already present, nothing to do.
+                    }
+                }
+
+                tags.InsertArrayElementAtIndex(tags.arraySize);
+                tags.GetArrayElementAtIndex(tags.arraySize - 1).stringValue = tag;
+                so.ApplyModifiedProperties();
+                so.Update();
+            }
+        }
+
+        public static void RemoveTag(string tag)
+        {
+            UnityEngine.Object[] asset = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset");
+            if ((asset != null) && (asset.Length > 0))
+            {
+                SerializedObject so = new SerializedObject(asset[0]);
+                SerializedProperty tags = so.FindProperty("tags");
+                int? index = null;
+
+                for (int i = 0; i < tags.arraySize; ++i)
+                {
+                    if (tags.GetArrayElementAtIndex(i).stringValue == tag)
+                    {
+                        index = i;
+                    }
+                }
+
+                if (index is int valueOfIndex)
+                {
+                    tags.DeleteArrayElementAtIndex(valueOfIndex);
+                    so.ApplyModifiedProperties();
+                    so.Update();
+                }
+
+            }
+        }
+
+    }
+}
