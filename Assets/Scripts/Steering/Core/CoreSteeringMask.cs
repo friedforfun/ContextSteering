a25f505b5edaa7c4540e94cac91143085ecce2d1
Sam Fay-Hunt <36762577+friedforfun@users.noreply.github.com>
2021-10-27 23:23:28 +0100

    Planar steering mask abstract class updates

--- Assets/Scripts/Steering/Core/CoreSteeringMask.cs
+++ Assets/Scripts/Steering/Core/CoreSteeringMask.cs
@@ -4,8 +4,11 @@ namespace Friedforfun.SteeringBehaviours.Core
 {
     public abstract class CoreSteeringMask<T> : MonoBehaviour where T: CoreSteeringParameters
     {
+        [Header("Mask Properties")]
         [Tooltip("Range at which the mask has an effect.")]
         [SerializeField] protected float Range;
+
+        public string MaskName;
 
         /// <summary>
         /// Instantiate the initial steering mask data
--- Assets/Scripts/Steering/PlanarMovement/PlanarSteeringMask.cs
+++ Assets/Scripts/Steering/PlanarMovement/PlanarSteeringMask.cs
@@ -23,12 +23,6 @@ namespace Friedforfun.SteeringBehaviours.PlanarMovement
         private NativeArray<Vector3> targetPositions;
         //private bool rebuildMap = false;
 
-        /// <summary>
-        /// Build a mask map where the index of the float defines the direction we wish to move, the size of the scalar defines how much we want to move in a direction
-        /// </summary>
-        /// <returns></returns>
-        public abstract float[] BuildMaskMap();
-
         public override void InstantiateMaskMap(PlanarSteeringParameters steeringParameters)
         {
             this.steeringParameters = steeringParameters;
