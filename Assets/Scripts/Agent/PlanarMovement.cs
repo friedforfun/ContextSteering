a0bb9a5c82b3aeebee927e5b3f7661362bb3d0f7
Sam Fay-Hunt <36762577+friedforfun@users.noreply.github.com>
2021-11-03 22:41:39 +0000

    Demo: Tweaked agent rotation and added confidence threshold

--- Assets/Scripts/Agent/PlanarMovement.cs
+++ Assets/Scripts/Agent/PlanarMovement.cs
@@ -11,19 +11,39 @@ namespace Friedforfun.SteeringBehaviours.Demo
     {
         [SerializeField] private PlanarSteeringController steer;
         [SerializeField] private CharacterController control;
-        [SerializeField] private GameObject LookTarget;
+        [SerializeField] public GameObject LookTarget;
 
+        [Tooltip("Movement speed of the agent.")]
         [Range(0.1f, 20f)]
         [SerializeField] private float Speed = 1f;
 
+        [Tooltip("Minimum sqrMagnitute of direction vector to allow movement, higher values can reduce jittery movement.")]
+        [Range(0.001f, 0.5f)]
+        [SerializeField] private float ConfidenceThreshold = 0.1f;
 
         void Update()
         {
-            // Apply movement based on direction obtained
-            control.SimpleMove(steer.MoveDirection() * Speed);
+
+            Vector3 moveVec = steer.MoveVector();
+            if (moveVec.sqrMagnitude > ConfidenceThreshold)
+                control.SimpleMove(steer.MoveDirection() * Speed);
+            else
+                control.SimpleMove(Vector3.zero);
 
             // Look towards target
-            Vector3 newRotation = Quaternion.LookRotation(MapOperations.VectorToTarget(gameObject, LookTarget).normalized).eulerAngles;
+            Vector3 newRotation;
+            if (LookTarget != null)
+            {
+                 newRotation = Quaternion.LookRotation(MapOperations.VectorToTarget(gameObject, LookTarget).normalized).eulerAngles;
+            }
+            else if (!moveVec.Equals(Vector3.zero))
+            {
+                newRotation = Quaternion.LookRotation(steer.MoveDirection()).eulerAngles;
+            } else
+            {
+                newRotation = Quaternion.LookRotation(transform.forward).eulerAngles;
+            }
+                
             transform.rotation = Quaternion.Euler(0, newRotation.y, 0);
         }
 
