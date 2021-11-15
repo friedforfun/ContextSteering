651932d4b558b758cd5a257bc26407cf94d77dd1
Sam Fay-Hunt <36762577+friedforfun@users.noreply.github.com>
2021-11-15 23:33:50 +0000

    Added check when stuck to recompute the path for navmesh demo

--- Samples/DemoScene/Scripts/Agent/Demo3NavmeshBuilder.cs
+++ Samples/DemoScene/Scripts/Agent/Demo3NavmeshBuilder.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Friedforfun.SteeringBehaviours.PlanarMovement;
+using Friedforfun.SteeringBehaviours.Utilities;
 
 namespace Friedforfun.SteeringBehaviours.Demo
 {
@@ -11,12 +12,18 @@ namespace Friedforfun.SteeringBehaviours.Demo
 
         private DotToNavmeshPath behaviour;
 
+        private float recomputePathThreshold = 2f;
+        private Vector3 lastPosition;
+
         private void Start()
         {
             behaviour = GetComponent<DotToNavmeshPath>();
-
+            lastPosition = transform.position;
             RecomputePath();
+            StartCoroutine(StuckCheck());
+
         }
+
 
         public void RecomputePath()
         {
@@ -24,6 +31,22 @@ namespace Friedforfun.SteeringBehaviours.Demo
             {
                 Debug.Log("Path Failed to build");
             }
+        }
+
+        private IEnumerator StuckCheck()
+        {
+            for (; ; )
+            {
+                yield return new WaitForSeconds(1);
+                if (MapOperations.VectorToTarget(transform.position, lastPosition).magnitude < recomputePathThreshold)
+                {
+                    //Debug.Log("Recomputing Path");
+                    RecomputePath();
+                }
+                lastPosition = transform.position;
+                    
+            }
+
         }
 
     }
