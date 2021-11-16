bd60ceef16b41fe6604782260ef2d9cc0c1223b1
Sam Fay-Hunt <36762577+friedforfun@users.noreply.github.com>
2021-11-16 01:06:16 +0000

    Added camera jump code

---
+++ Samples/DemoScene/Scripts/TestLevel/CameraControls.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform[] DemoTransforms;
+
+    private Transform targetTransform;
+    private float transitionTime = 0f;
+    private Vector3 StartPosition;
+    private bool inputDiverted = true;
+
+
+    private void SelectDemoView(int numKeyPress)
+    {
+        inputDiverted = false;
+        transitionTime = 0f;
+        StartPosition = transform.position;
+
+        if (DemoTransforms == null)
+            return;
+
+        if (numKeyPress > DemoTransforms.Length || numKeyPress == 0)
+            return;
+
+        targetTransform = DemoTransforms[numKeyPress - 1];
+    }
+
+    private void LerpToTarget()
+    {
+        transform.position = Vector3.Lerp(StartPosition, targetTransform.position, transitionTime);
+        transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, targetTransform.rotation.eulerAngles, transitionTime));
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Num1"))
+            SelectDemoView(1);
+
+        if (Input.GetButtonDown("Num2"))
+            SelectDemoView(2);
+
+        if (Input.GetButtonDown("Num3"))
+            SelectDemoView(3);
+
+        if (Input.GetButtonDown("Num4"))
+            SelectDemoView(4);
+
+        transitionTime += Time.deltaTime;
+
+        if (!inputDiverted)
+            LerpToTarget();
+    }
+
+}
